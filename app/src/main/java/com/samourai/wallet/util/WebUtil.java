87cb69bc18e2c193ec7637a3130341d393a8d57d
TDevD <dev@samouraiwallet.com>
2016-05-11 10:49:58 +0100

    Use Samourai SSL

--- app/src/main/java/com/samourai/wallet/util/WebUtil.java
+++ app/src/main/java/com/samourai/wallet/util/WebUtil.java
@@ -20,13 +20,13 @@ import info.guardianproject.onionkit.ui.OrbotHelper;
 public class WebUtil	{
 
     public static final String BLOCKCHAIN_DOMAIN = "https://blockchain.info/";
-    public static final String SAMOURAI_API = "http://api.samouraiwallet.com:8080/";
+    public static final String SAMOURAI_API = "https://api.samouraiwallet.com/";
 
     public static final String LBC_EXCHANGE_URL = "https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/";
     public static final String BTCe_EXCHANGE_URL = "https://btc-e.com/api/3/ticker/";
     public static final String BFX_EXCHANGE_URL = "https://api.bitfinex.com/v1/pubticker/btcusd";
     public static final String AVG_EXCHANGE_URL = "https://api.bitcoinaverage.com/ticker/global/all";
-    public static final String VALIDATE_SSL_URL = BLOCKCHAIN_DOMAIN;
+    public static final String VALIDATE_SSL_URL = SAMOURAI_API;
 
     public static final String BTCX_FEE_URL = "http://bitcoinexchangerate.org/fees";
 
