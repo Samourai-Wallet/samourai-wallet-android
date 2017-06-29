23c5517d5d9e7b80a3bd9c399d736f085757ed7e
TDevD <dev@samouraiwallet.com>
2017-06-29 09:51:44 +0200

    read USB FAT12

---
+++ app/src/main/java/com/samourai/wallet/OpenDimeActivity.java
@@ -0,0 +1,24 @@
+package com.samourai.wallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.samourai.wallet.util.AppUtil;
+
+public class OpenDimeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setTitle("Samourai, v" + getResources().getString(R.string.version_name));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        AppUtil.getInstance(OpenDimeActivity.this).checkTimeOut();
+
+    }
+}
