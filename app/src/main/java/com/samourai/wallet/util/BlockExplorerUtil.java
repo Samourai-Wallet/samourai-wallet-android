2b81e86df864a2829202ab8bd48f0ea4f60f0463
TDevD <dev@samouraiwallet.com>
2017-07-11 18:57:08 +0200

    remove Bisq block explorer

--- app/src/main/java/com/samourai/wallet/util/BlockExplorerUtil.java
+++ app/src/main/java/com/samourai/wallet/util/BlockExplorerUtil.java
@@ -2,9 +2,9 @@ package com.samourai.wallet.util;
 
 public class BlockExplorerUtil {
 
-    private static CharSequence[] blockExplorers = { "Smartbit", "UASF Explorer", "Bisq"  };
-    private static CharSequence[] blockExplorerTxUrls = { "https://www.smartbit.com.au/tx/", "https://uasf-explorer.satoshiportal.com/tx/", "https://explorer.bisq.io:8443/insight/tx/" };
-    private static CharSequence[] blockExplorerAddressUrls = { "https://www.smartbit.com.au/address/", "https://uasf-explorer.satoshiportal.com/address/", "https://explorer.bisq.io:8443/insight/address/" };
+    private static CharSequence[] blockExplorers = { "Smartbit", "UASF Explorer" };
+    private static CharSequence[] blockExplorerTxUrls = { "https://www.smartbit.com.au/tx/", "https://uasf-explorer.satoshiportal.com/tx/" };
+    private static CharSequence[] blockExplorerAddressUrls = { "https://www.smartbit.com.au/address/", "https://uasf-explorer.satoshiportal.com/address/" };
 
     private static BlockExplorerUtil instance = null;
 
