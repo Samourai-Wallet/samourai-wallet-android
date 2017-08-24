73f629dafcb1dc89ef1d8a0ce79880ddd87f232c
TDevD <dev@samouraiwallet.com>
2017-08-24 07:38:12 +0200

    add support for yogh.io block explorer

--- app/src/main/java/com/samourai/wallet/util/BlockExplorerUtil.java
+++ app/src/main/java/com/samourai/wallet/util/BlockExplorerUtil.java
@@ -2,9 +2,9 @@ package com.samourai.wallet.util;
 
 public class BlockExplorerUtil {
 
-    private static CharSequence[] blockExplorers = { "Smartbit", "UASF Explorer" };
-    private static CharSequence[] blockExplorerTxUrls = { "https://www.smartbit.com.au/tx/", "https://uasf-explorer.satoshiportal.com/tx/" };
-    private static CharSequence[] blockExplorerAddressUrls = { "https://www.smartbit.com.au/address/", "https://uasf-explorer.satoshiportal.com/address/" };
+    private static CharSequence[] blockExplorers = { "Smartbit", "UASF Explorer", "Blockchain Reader (Yogh)" };
+    private static CharSequence[] blockExplorerTxUrls = { "https://www.smartbit.com.au/tx/", "https://uasf-explorer.satoshiportal.com/tx/", "http://srv1.yogh.io/#tx:id:" };
+    private static CharSequence[] blockExplorerAddressUrls = { "https://www.smartbit.com.au/address/", "https://uasf-explorer.satoshiportal.com/address/", "http://srv1.yogh.io/#addr:id:" };
 
     private static BlockExplorerUtil instance = null;
 
