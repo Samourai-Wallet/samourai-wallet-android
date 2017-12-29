a2de778b2ecd5d02e35271625a65a5f8622439be
TDevD <dev@samouraiwallet.com>
2017-12-29 09:06:15 +0100

    check for out-of-range fee provider selection

--- app/src/main/java/com/samourai/wallet/payload/PayloadUtil.java
+++ app/src/main/java/com/samourai/wallet/payload/PayloadUtil.java
@@ -483,7 +483,7 @@ public class PayloadUtil	{
                     PrefsUtil.getInstance(context).setValue(PrefsUtil.USE_TRUSTED_NODE, meta.getBoolean("use_trusted_node"));
                 }
                 if(meta.has("fee_provider_sel")) {
-                    PrefsUtil.getInstance(context).setValue(PrefsUtil.FEE_PROVIDER_SEL, meta.getInt("fee_provider_sel"));
+                    PrefsUtil.getInstance(context).setValue(PrefsUtil.FEE_PROVIDER_SEL, meta.getInt("fee_provider_sel") > 1 ? 0 : meta.getInt("fee_provider_sel"));
                 }
                 if(meta.has("broadcast_tx")) {
                     PrefsUtil.getInstance(context).setValue(PrefsUtil.BROADCAST_TX, meta.getBoolean("broadcast_tx"));
