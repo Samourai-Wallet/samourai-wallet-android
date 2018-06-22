ca8154b5fd2df71444000a19cd732d4922cacbbf
Sarath kumar <sarathkumars677@gmail.com>
2018-06-22 11:29:23 +0530

    [WIP] implementing Send UI

---
+++ app/src/main/java/com/samourai/wallet/SendNewUIActivity.java
@@ -0,0 +1,16 @@
+package com.samourai.wallet;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SendNewUIActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_new_ui);
+    }
+
+}
