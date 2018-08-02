80a01007c35fa702f11949ed7aebfce7c6c2cb53
Sarath kumar <sarathkumars677@gmail.com>
2018-08-02 22:08:39 +0530

    [WIP] Whirlpool base activity

---
+++ app/src/main/java/com/samourai/wallet/whirlpool/WhirlPoolActivity.java
@@ -0,0 +1,15 @@
+package com.samourai.wallet.whirlpool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.samourai.wallet.R;
+
+public class WhirlPoolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whirl_pool);
+    }
+}
