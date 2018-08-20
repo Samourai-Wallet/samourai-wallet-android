b4602d695d4ef2ef0bfc6f3b753d4fb7170861f9
Sarath kumar <sarathkumars677@gmail.com>
2018-08-20 18:33:32 +0530

    [WIP] Whirlpool UI Main Activity and minor improvements

---
+++ app/src/main/java/com/samourai/wallet/whirlpool/WhirlpoolMain.java
@@ -0,0 +1,16 @@
+package com.samourai.wallet.whirlpool;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.samourai.wallet.R;
+
+public class WhirlpoolMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_whirlpool_main);
+    }
+
+}
