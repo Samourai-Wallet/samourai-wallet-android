11d66f8e55506ef1a91ec5a5eff21cdb01a6f76d
Sarath kumar <sarathkumars677@gmail.com>
2018-11-22 20:20:45 +0530

    Btc and sat conversion

--- app/src/main/java/com/samourai/wallet/spend/SendNewUIActivity.java
+++ app/src/main/java/com/samourai/wallet/spend/SendNewUIActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,16 +18,22 @@ import com.samourai.wallet.R;
 import com.samourai.wallet.spend.widgets.EntropyBar;
 import com.samourai.wallet.spend.widgets.SendTransactionDetailsView;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Locale;
+
 
 public class SendNewUIActivity extends AppCompatActivity {
 
     private SendTransactionDetailsView sendTransactionDetailsView;
     private ViewSwitcher amountViewSwitcher;
-    private EditText toEditText, btcEditText, fiatEditText;
+    private EditText toEditText, btcEditText, satEditText;
     private Button btnReview, btnSend;
     private Switch ricochetHopsSwitch;
     private Boolean isOnReviewPage = false;
     private EntropyBar entropyBar;
+    private static final String TAG = "SendNewUIActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +53,9 @@ public class SendNewUIActivity extends AppCompatActivity {
         //Input elements from toolbar section of the UI
         toEditText = findViewById(R.id.edt_send_to);
         btcEditText = findViewById(R.id.amountBTC);
-        fiatEditText = findViewById(R.id.amountSat);
+        satEditText = findViewById(R.id.amountSat);
+        btcEditText.addTextChangedListener(BTCWatcher);
+        satEditText.addTextChangedListener(satWatcher);
 
         entropyBar = sendTransactionDetailsView.getTransactionReview().findViewById(R.id.entropyBar);
         //view elements from review segment and transaction segment can be access through respective
@@ -63,6 +74,84 @@ public class SendNewUIActivity extends AppCompatActivity {
 
     }
 
+    private TextWatcher BTCWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            satEditText.removeTextChangedListener(satWatcher);
+            btcEditText.removeTextChangedListener(this);
+
+            try {
+                String cleared_comma = editable.toString().replace(",", "");
+                Float btc = Float.parseFloat(cleared_comma);
+                Double sats = getSatValue(btc);
+                BigDecimal numberBigDecimal = new BigDecimal(btc);
+//                Log.i(TAG, "afterTextChanged: ".concat(String.vabtc.longValue()));
+                DecimalFormat df = new DecimalFormat("#,###,###,###");
+                satEditText.setText(df.format(sats));
+//                btcEditText.setText(String.valueOf(numberBigDecimal));
+//                btcEditText.setSelection(String.valueOf(numberBigDecimal).length());
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            satEditText.addTextChangedListener(satWatcher);
+            btcEditText.addTextChangedListener(this);
+
+
+        }
+    };
+
+    private TextWatcher satWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            satEditText.removeTextChangedListener(this);
+            btcEditText.removeTextChangedListener(BTCWatcher);
+
+            try {
+                String cleared_comma = editable.toString().replace(",", "");
+                Double sats = Double.parseDouble(cleared_comma);
+                Float btc = getBtcValue(sats);
+//                if(sats)
+                BigDecimal numberBigDecimal = new BigDecimal(btc);
+
+//                Log.i(TAG, "afterTextChanged: ".concat(String.vabtc.longValue()));
+                DecimalFormat df = new DecimalFormat("#,###,###,###");
+                satEditText.setText(df.format(sats));
+                satEditText.setSelection(df.format(sats).length());
+                btcEditText.setText(String.format(Locale.ENGLISH, "%f", numberBigDecimal.floatValue()));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+            satEditText.addTextChangedListener(this);
+            btcEditText.addTextChangedListener(BTCWatcher);
+
+        }
+    };
+
+
+    private Float getBtcValue(Double sats) {
+        return (float) (sats / 100000000);
+    }
+
+    private Double getSatValue(Float btc) {
+        return Double.valueOf(btc * 100000000);
+    }
+
     private void review() {
         amountViewSwitcher.showNext();
         sendTransactionDetailsView.showReview(ricochetHopsSwitch.isChecked());
@@ -73,19 +162,20 @@ public class SendNewUIActivity extends AppCompatActivity {
             public void run() {
                 entropyBar.disable();
             }
-        },4000);
+        }, 4000);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 entropyBar.setRange(3);
             }
-        },7000);  new Handler().postDelayed(new Runnable() {
+        }, 7000);
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 entropyBar.setRange(2);
             }
-        },9000);
+        }, 9000);
     }
 
     private void backToTransactionView() {
