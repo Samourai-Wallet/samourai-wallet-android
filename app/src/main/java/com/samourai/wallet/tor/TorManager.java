126d25389971a051f24faedf802461060b909ebf
Sarath kumar <sarathkumars677@gmail.com>
2019-02-21 00:26:26 +0530

    Updated Tor manager and services
    * added Tor state changes 
    *stream logs to notification
    *Tor service with sticky notifications

--- app/src/main/java/com/samourai/wallet/tor/TorManager.java
+++ app/src/main/java/com/samourai/wallet/tor/TorManager.java
@@ -2,27 +2,40 @@ package com.samourai.wallet.tor;
 
 
 import android.content.Context;
+import android.util.Log;
 
 import com.msopentech.thali.android.toronionproxy.AndroidOnionProxyManager;
 import com.msopentech.thali.toronionproxy.OnionProxyManager;
 
+import java.io.IOException;
+import java.net.ConnectException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
+import java.net.Socket;
 
 import io.reactivex.Observable;
 import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.Subject;
 
 public class TorManager {
+    private static final String TAG = "TorManager";
+
+    public enum CONNECTION_STATES {
+        CONNECTED,
+        DISCONNECTED,
+        CONNECTING
+    }
 
     static TorManager instance;
 
 
     private Context context;
     private Proxy proxy = null;
-    public Subject<Boolean> torStatus = PublishSubject.create();
-    private boolean isTorConnecting;
+    public CONNECTION_STATES state = CONNECTION_STATES.DISCONNECTED;
+    public Subject<CONNECTION_STATES> torStatus = PublishSubject.create();
     OnionProxyManager onionProxyManager;
+    public boolean isProcessRunning = false;
+    String fileStorageLocation = "torfiles";
 
     public static TorManager getInstance(Context context) {
         if (instance == null) {
@@ -33,16 +46,15 @@ public class TorManager {
 
     private TorManager(Context context) {
         this.context = context;
-        torStatus.onNext(false);
-        String fileStorageLocation = "torfiles";
+        torStatus.onNext(CONNECTION_STATES.DISCONNECTED);
         onionProxyManager = new AndroidOnionProxyManager(context, fileStorageLocation);
-
     }
 
     public Observable<Proxy> startTor() {
-
+        Log.i(TAG, "startTor: ");
         return Observable.fromCallable(() -> {
-            isTorConnecting = true;
+            state = CONNECTION_STATES.CONNECTING;
+            torStatus.onNext(CONNECTION_STATES.CONNECTING);
 
             int totalSecondsPerTorStartup = 4 * 60;
             int totalTriesPerTorStartup = 5;
@@ -54,43 +66,81 @@ public class TorManager {
                 }
                 while (!onionProxyManager.isRunning())
                     Thread.sleep(90);
-                System.out.println("Tor initialized on port " + onionProxyManager.getIPv4LocalHostSocksPort());
                 proxy = new Proxy(Proxy.Type.SOCKS, new InetSocketAddress("127.0.0.1", onionProxyManager.getIPv4LocalHostSocksPort()));
                 if (torStatus.hasObservers()) {
-                    torStatus.onNext(true);
+                    torStatus.onNext(CONNECTION_STATES.CONNECTED);
                 }
-                isTorConnecting = false;
+                isProcessRunning = true;
+                state = CONNECTION_STATES.CONNECTED;
+
                 return proxy;
             } catch (Exception e) {
-                isTorConnecting = false;
+                e.printStackTrace();
+                state = CONNECTION_STATES.DISCONNECTED;
                 if (torStatus.hasObservers()) {
-                    torStatus.onNext(false);
+                    torStatus.onNext(CONNECTION_STATES.DISCONNECTED);
                 }
                 e.printStackTrace();
-                return null;
+                return proxy;
             }
         });
 
     }
 
     public String getLatestLogs() {
-        if (onionProxyManager != null)
-            return onionProxyManager.getLastLog();
-        else
+        try {
+            if (onionProxyManager != null && onionProxyManager.isRunning()) {
+                String log = onionProxyManager.getLastLog();
+                try {
+                    if (!TorManager.isPortOpen("127.0.0.1", onionProxyManager.getIPv4LocalHostSocksPort(), 4000)) {
+                        this.state = CONNECTION_STATES.DISCONNECTED;
+                        if (torStatus.hasObservers()) {
+                            torStatus.onNext(CONNECTION_STATES.DISCONNECTED);
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.i(TAG, "getLatestLogs: LOG");
+                    e.printStackTrace();
+                }
+                return log;
+            } else {
+                return "";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
             return "";
+        }
     }
 
-    Proxy getProxy() {
+    public boolean isConnected() {
+        Log.i(TAG, "isConnected: ");
+        return this.state == CONNECTION_STATES.CONNECTED;
+    }
 
+    public Proxy getProxy() {
         return proxy;
     }
 
     public Observable<Boolean> stopTor() {
-        torStatus.onNext(false);
+        Log.i(TAG, "stopTor: ");
+        if (torStatus.hasObservers()) {
+            torStatus.onNext(CONNECTION_STATES.DISCONNECTED);
+            state = CONNECTION_STATES.DISCONNECTED;
+        }
         return Observable.fromCallable(() -> {
             try {
-                onionProxyManager.stop();
+                this.state = CONNECTION_STATES.DISCONNECTED;
+                if (torStatus.hasObservers()) {
+                    torStatus.onNext(CONNECTION_STATES.DISCONNECTED);
+                }
+                isProcessRunning = false;
             } catch (Exception ex) {
+                ex.printStackTrace();
+                this.state = CONNECTION_STATES.DISCONNECTED;
+                if (torStatus.hasObservers()) {
+                    torStatus.onNext(CONNECTION_STATES.DISCONNECTED);
+                }
+                isProcessRunning = false;
                 return false;
             }
 
@@ -99,7 +149,7 @@ public class TorManager {
 
     }
 
-    public Subject<Boolean> getTorStatus() {
+    public Subject<CONNECTION_STATES> getTorStatus() {
         return torStatus;
     }
 
@@ -118,5 +168,30 @@ public class TorManager {
         });
 
     }
+
+
+    public void setTorState(CONNECTION_STATES state) {
+        this.state = state;
+        if (torStatus.hasObservers()) {
+            torStatus.onNext(state);
+        }
+    }
+
+    public static boolean isPortOpen(final String ip, final int port, final int timeout) {
+
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress(ip, port), timeout);
+            socket.close();
+            return true;
+        } catch (ConnectException ce) {
+            ce.printStackTrace();
+            return false;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+
 
 }
