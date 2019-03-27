466cba56fb9851a1fa4e3427d886d361d4e755b6
Sarath kumar <sarathkumars677@gmail.com>
2019-03-27 17:50:24 +0530

    fix conflicts

--- app/src/main/java/com/samourai/wallet/bip47/paynym/WebUtil.java
+++ app/src/main/java/com/samourai/wallet/bip47/paynym/WebUtil.java
@@ -1,37 +1,17 @@
 package com.samourai.wallet.bip47.paynym;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.samourai.wallet.BuildConfig;
-import com.samourai.wallet.R;
 import com.samourai.wallet.tor.TorManager;
-import com.samourai.wallet.util.TorUtil;
 
-import org.apache.commons.io.IOUtils;
+import java.util.concurrent.TimeUnit;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import ch.boye.httpclientandroidlib.HttpResponse;
-import ch.boye.httpclientandroidlib.NameValuePair;
-import ch.boye.httpclientandroidlib.client.entity.UrlEncodedFormEntity;
-import ch.boye.httpclientandroidlib.client.methods.HttpDelete;
-import ch.boye.httpclientandroidlib.client.methods.HttpGet;
-import ch.boye.httpclientandroidlib.client.methods.HttpPost;
-import ch.boye.httpclientandroidlib.message.BasicNameValuePair;
 import info.guardianproject.netcipher.client.StrongHttpsClient;
-import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 
@@ -63,84 +43,45 @@ public class WebUtil {
         return instance;
     }
 
-    public String postURL(String request, String urlParameters) throws Exception {
-
-        if (context == null) {
-            return postURL(null, null, request, urlParameters);
-        } else {
-            Log.i("WebUtil", "Tor enabled status:" + TorUtil.getInstance(context).statusFromBroadcast());
-            if (TorUtil.getInstance(context).statusFromBroadcast()) {
-                if (urlParameters.startsWith("tx=")) {
-                    HashMap<String, String> args = new HashMap<String, String>();
-                    args.put("tx", urlParameters.substring(3));
-                    return tor_postURL(request, args);
-                } else {
-                    return tor_postURL(request + urlParameters, null);
-                }
-            } else {
-                return postURL(null, null, request, urlParameters);
-            }
-
-        }
-
-    }
-
     public String postURL(String contentType, String authToken, String requestURL, String urlParameters) throws Exception {
 
-        String error = null;
+        MediaType JSON
+                = MediaType.parse("application/json; charset=utf-8");
 
-        FormBody.Builder formBodyBuilder = new FormBody.Builder();
+        RequestBody body = RequestBody.create(JSON, urlParameters);
 
-        OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                .proxy(TorManager.getInstance(this.context).getProxy());
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+
+
+        builder.callTimeout(DefaultRequestTimeout, TimeUnit.MILLISECONDS);
 
         if (BuildConfig.DEBUG) {
             builder.addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
         }
-        Request request = new Request.Builder()
+        Request.Builder rbuilder = new Request.Builder()
                 .url(requestURL)
-                .addHeader("Content-Type", contentType == null ? "application/x-www-form-urlencoded" : contentType)
-                .post(formBodyBuilder.build())
+                .addHeader("Content-Type", contentType == null ? "application/x-www-form-urlencoded" : contentType);
+
+        if (authToken != null) {
+            rbuilder.addHeader("auth-token", authToken);
+        }
+        if(TorManager.getInstance(context).isConnected()){
+            builder.proxy(TorManager.getInstance(this.context).getProxy());
+        }
+
+        Request request = rbuilder
+                .post(body)
                 .build();
 
         try (Response response = builder.build().newCall(request).execute()) {
-            if(response.body() == null){
-                return  "";
-            }
-            return response.body().string();
-        }
-
-     }
-
-    public String tor_postURL(String URL, HashMap<String, String> args) throws Exception {
-
-        FormBody.Builder formBodyBuilder = new FormBody.Builder();
-
-        if (args != null) {
-            List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-            for (String key : args.keySet()) {
-                formBodyBuilder.add(key, args.get(key));
-            }
-        }
-
-        OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                .proxy(TorManager.getInstance(this.context).getProxy());
-
-        if (BuildConfig.DEBUG) {
-            builder.addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
-        }
-        Request request = new Request.Builder()
-                .url(URL)
-                .post(formBodyBuilder.build())
-                .build();
-
-        try (Response response = builder.build().newCall(request).execute()) {
-            if(response.body() == null){
-                return  "";
+            if (response.body() == null) {
+                return "";
             }
             return response.body().string();
         }
 
     }
+
+
 
 }
