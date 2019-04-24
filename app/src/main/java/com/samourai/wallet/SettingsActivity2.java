1aadcd403fdb0378f1a286dcb8ece863082c5e80
ottosch <ottosch@protonmail.com>
2019-04-23 23:24:08 -0300

    Fixed mistake in settings activity

--- app/src/main/java/com/samourai/wallet/SettingsActivity2.java
+++ app/src/main/java/com/samourai/wallet/SettingsActivity2.java
@@ -580,8 +580,8 @@ public class SettingsActivity2 extends PreferenceActivity	{
                     }
                 });
 
-                final CheckBoxPreference cbPrefPinConfirm = (CheckBoxPreference) findPreference("pinConfirm");
-                cbPref11.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                final CheckBoxPreference cbPrefPinConfirm = (CheckBoxPreference) findPreference("confirmPin");
+                cbPrefPinConfirm.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
                     public boolean onPreferenceChange(Preference preference, Object newValue) {
                         PrefsUtil.getInstance(SettingsActivity2.this).setValue(PrefsUtil.CONFIRM_PIN, !cbPrefPinConfirm.isChecked());
                         return true;
