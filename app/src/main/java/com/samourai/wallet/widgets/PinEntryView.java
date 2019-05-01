ef0302fe0cfa6ef91a437fdfddd4296c9b5b68b0
Sarath kumar <sarathkumars677@gmail.com>
2019-05-01 15:00:38 +0530

    Rewrite keypad view with constraint layout

--- app/src/main/java/com/samourai/wallet/widgets/PinEntryView.java
+++ app/src/main/java/com/samourai/wallet/widgets/PinEntryView.java
@@ -1,12 +1,18 @@
 package com.samourai.wallet.widgets;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.os.Vibrator;
+import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import com.samourai.wallet.R;
 import com.samourai.wallet.access.AccessFactory;
@@ -19,6 +25,7 @@ import static android.content.Context.VIBRATOR_SERVICE;
  * Re-Usable custom KeypadView for pin entry
  */
 public class PinEntryView extends FrameLayout implements View.OnClickListener {
+
 
 
     public enum KeyClearTypes {
@@ -36,7 +43,7 @@ public class PinEntryView extends FrameLayout implements View.OnClickListener {
     private Button th = null;
     private Button ti = null;
     private Button tj = null;
-    private ImageButton tsend = null;
+    private ImageButton tconfirm = null;
     private ImageButton tback = null;
     private ScrambledPin keypad = null;
     private int pinLen = 0;
@@ -45,6 +52,7 @@ public class PinEntryView extends FrameLayout implements View.OnClickListener {
     private pinClearListener clearListener = null;
     private Vibrator vibrator;
     private boolean enableHaptic = true;
+    private ConstraintLayout gridLayout;
 
     public PinEntryView(Context context) {
         super(context);
@@ -77,39 +85,34 @@ public class PinEntryView extends FrameLayout implements View.OnClickListener {
     private void initView() {
         vibrator = (Vibrator) getContext().getSystemService(VIBRATOR_SERVICE);
         View view = inflate(getContext(), R.layout.keypad_view, null);
-        ta = (Button) view.findViewById(R.id.ta);
-        tb = (Button) view.findViewById(R.id.tb);
-        tc = (Button) view.findViewById(R.id.tc);
-        td = (Button) view.findViewById(R.id.td);
-        te = (Button) view.findViewById(R.id.te);
-        tf = (Button) view.findViewById(R.id.tf);
-        tg = (Button) view.findViewById(R.id.tg);
-        th = (Button) view.findViewById(R.id.th);
-        ti = (Button) view.findViewById(R.id.ti);
-        tj = (Button) view.findViewById(R.id.tj);
-        tsend = (ImageButton) view.findViewById(R.id.tsend);
-        tback = (ImageButton) view.findViewById(R.id.tback);
-        tback.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hapticFeedBack();
-                pinLen = pinLen--;
+        gridLayout = view.findViewById(R.id.pin_entry_grid);
+        ta = view.findViewById(R.id.ta);
+        tb = view.findViewById(R.id.tb);
+        tc = view.findViewById(R.id.tc);
+        td = view.findViewById(R.id.td);
+        te = view.findViewById(R.id.te);
+        tf = view.findViewById(R.id.tf);
+        tg = view.findViewById(R.id.tg);
+        th = view.findViewById(R.id.th);
+        ti = view.findViewById(R.id.ti);
+        tj = view.findViewById(R.id.tj);
+        tconfirm = view.findViewById(R.id.tconfirm);
+        tback = view.findViewById(R.id.tback);
+        tback.setOnClickListener(view1 -> {
+            hapticFeedBack();
+            pinLen = pinLen--;
 
-                if (clearListener != null) {
-                    clearListener.onPinClear(KeyClearTypes.CLEAR);
-                }
+            if (clearListener != null) {
+                clearListener.onPinClear(KeyClearTypes.CLEAR);
             }
         });
-        tback.setOnLongClickListener(new OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                pinLen = 0;
-                if (clearListener != null) {
-                    clearListener.onPinClear(KeyClearTypes.CLEAR_ALL);
-                }
-                hapticFeedBack();
-                return false;
+        tback.setOnLongClickListener(view12 -> {
+            pinLen = 0;
+            if (clearListener != null) {
+                clearListener.onPinClear(KeyClearTypes.CLEAR_ALL);
             }
+            hapticFeedBack();
+            return false;
         });
         setButtonLabels();
         addView(view);
@@ -131,6 +134,32 @@ public class PinEntryView extends FrameLayout implements View.OnClickListener {
         if (this.enableHaptic) {
             vibrator.vibrate(44);
         }
+    }
+
+
+    public void showCheckButton() {
+        tconfirm.setVisibility(VISIBLE);
+    }
+
+    public void hideCheckButton() {
+        tconfirm.setVisibility(GONE);
+
+    }
+
+    public void setConfirmClickListner(OnClickListener clickListner){
+        tconfirm.setOnClickListener(clickListner);
+    }
+
+    public void setSize(int size,int margin) {
+//        int dp = (int) (getResources().getDimension(size));
+//        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+//            GridLayout.LayoutParams params = new GridLayout.LayoutParams(new ViewGroup.LayoutParams(dp, dp));
+//            params.setMargins(margin,margin,margin,margin);
+//            gridLayout.getChildAt(i).setLayoutParams(params);
+//        }
+    }
+    public void disableHapticFeedBack(){
+        enableHaptic = false;
     }
 
     private void setButtonLabels() {
