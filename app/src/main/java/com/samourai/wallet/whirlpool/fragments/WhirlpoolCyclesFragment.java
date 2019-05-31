247ca820a1bde49adb2ae9d4564e24ad12cc11da
Sarath kumar <sarathkumars677@gmail.com>
2019-05-31 18:53:18 +0530

    [WIP] whirlpool Cycle list adapter implementation

--- app/src/main/java/com/samourai/wallet/whirlpool/fragments/WhirlpoolCyclesFragment.java
+++ app/src/main/java/com/samourai/wallet/whirlpool/fragments/WhirlpoolCyclesFragment.java
@@ -1,9 +1,9 @@
 package com.samourai.wallet.whirlpool.fragments;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -11,43 +11,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.samourai.wallet.R;
-import com.samourai.wallet.whirlpool.fragments.dummy.DummyContent;
-import com.samourai.wallet.whirlpool.fragments.dummy.DummyContent.DummyItem;
+import com.samourai.wallet.whirlpool.models.Cycle;
+import com.samourai.wallet.widgets.ItemDividerDecorator;
 
+import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
 public class WhirlpoolCyclesFragment extends Fragment {
 
-    private static final String ARG_COLUMN_COUNT = "column-count";
-    private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
-
+    private WhirlpoolCyclesFragmentAdapter adapter;
+    private RecyclerView cyclesList;
     public WhirlpoolCyclesFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static WhirlpoolCyclesFragment newInstance(int columnCount) {
-        WhirlpoolCyclesFragment fragment = new WhirlpoolCyclesFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getArguments() != null) {
-            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
-        }
     }
 
     @Override
@@ -55,16 +36,14 @@ public class WhirlpoolCyclesFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.whirlpool_cycles_list, container, false);
 
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            if (mColumnCount <= 1) {
-                recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            } else {
-                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
-            }
-            recyclerView.setAdapter(new WhirlpoolCyclesFragmentAdapter(DummyContent.ITEMS, mListener));
-        }
+        adapter = new WhirlpoolCyclesFragmentAdapter(new ArrayList<>());
+
+        cyclesList = view.findViewById(R.id.whirlpool_cycles_rv);
+        cyclesList.setLayoutManager(new LinearLayoutManager(getContext()));
+        cyclesList.setAdapter(adapter);
+        Drawable drawable = this.getResources().getDrawable(R.drawable.divider_grey);
+
+        cyclesList.addItemDecoration(new ItemDividerDecorator(drawable));
         return view;
     }
 
@@ -72,32 +51,49 @@ public class WhirlpoolCyclesFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        if (context instanceof OnListFragmentInteractionListener) {
-//            mListener = (OnListFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnListFragmentInteractionListener");
-//        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(DummyItem item);
+
+    private class WhirlpoolCyclesFragmentAdapter extends RecyclerView.Adapter<WhirlpoolCyclesFragmentAdapter.ViewHolder> {
+
+
+        public WhirlpoolCyclesFragmentAdapter(List<Cycle> items) {
+
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.cycle_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 5;
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            final View mView;
+
+
+            ViewHolder(View view) {
+                super(view);
+                mView = view;
+
+            }
+
+        }
     }
+
 }
