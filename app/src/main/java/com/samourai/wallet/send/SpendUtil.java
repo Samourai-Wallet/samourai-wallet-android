36ab13f336c1f46956a5d4e28c9e38604e6b4856
TDevD <dev@samouraiwallet.com>
2019-09-13 15:35:39 +0200

    SpendUtil: use updated UTXOFactory

--- app/src/main/java/com/samourai/wallet/send/SpendUtil.java
+++ app/src/main/java/com/samourai/wallet/send/SpendUtil.java
@@ -20,20 +20,20 @@ public class SpendUtil {
         List<UTXO> utxos = null;
         if(account == WhirlpoolMeta.getInstance(context).getWhirlpoolPostmix())    {
             if(UTXOFactory.getInstance().getTotalPostMix() > neededAmount)    {
-                utxos = new ArrayList<UTXO>(UTXOFactory.getInstance().getPostMix().values());
+                utxos = new ArrayList<UTXO>(UTXOFactory.getInstance().getAllPostMix().values());
             }
             else if(account == WhirlpoolMeta.getInstance(context).getWhirlpoolPostmix())    {
                 return null;
             }
         }
-        else if (FormatsUtil.getInstance().isValidBech32(address) && (UTXOFactory.getInstance().getP2WPKH().size() > 0 && UTXOFactory.getInstance().getTotalP2WPKH() > neededAmount)) {
-            utxos = new ArrayList<UTXO>(UTXOFactory.getInstance().getP2WPKH().values());
+        else if (FormatsUtil.getInstance().isValidBech32(address) && (UTXOFactory.getInstance().getAllP2WPKH().size() > 0 && UTXOFactory.getInstance().getTotalP2WPKH() > neededAmount)) {
+            utxos = new ArrayList<UTXO>(UTXOFactory.getInstance().getAllP2WPKH().values());
 //                    Log.d("SendActivity", "segwit utxos:" + utxos.size());
-        } else if (!FormatsUtil.getInstance().isValidBech32(address) && Address.fromBase58(SamouraiWallet.getInstance().getCurrentNetworkParams(), address).isP2SHAddress() && (UTXOFactory.getInstance().getP2SH_P2WPKH().size() > 0 && UTXOFactory.getInstance().getTotalP2SH_P2WPKH() > neededAmount)) {
-            utxos = new ArrayList<UTXO>(UTXOFactory.getInstance().getP2SH_P2WPKH().values());
+        } else if (!FormatsUtil.getInstance().isValidBech32(address) && Address.fromBase58(SamouraiWallet.getInstance().getCurrentNetworkParams(), address).isP2SHAddress() && (UTXOFactory.getInstance().getAllP2SH_P2WPKH().size() > 0 && UTXOFactory.getInstance().getTotalP2SH_P2WPKH() > neededAmount)) {
+            utxos = new ArrayList<UTXO>(UTXOFactory.getInstance().getAllP2SH_P2WPKH().values());
 //                    Log.d("SendActivity", "segwit utxos:" + utxos.size());
-        } else if ((!FormatsUtil.getInstance().isValidBech32(address) && !Address.fromBase58(SamouraiWallet.getInstance().getCurrentNetworkParams(), address).isP2SHAddress()) && (UTXOFactory.getInstance().getP2PKH().size() > 0) && (UTXOFactory.getInstance().getTotalP2PKH() > neededAmount)) {
-            utxos = new ArrayList<UTXO>(UTXOFactory.getInstance().getP2PKH().values());
+        } else if ((!FormatsUtil.getInstance().isValidBech32(address) && !Address.fromBase58(SamouraiWallet.getInstance().getCurrentNetworkParams(), address).isP2SHAddress()) && (UTXOFactory.getInstance().getAllP2PKH().size() > 0) && (UTXOFactory.getInstance().getTotalP2PKH() > neededAmount)) {
+            utxos = new ArrayList<UTXO>(UTXOFactory.getInstance().getAllP2PKH().values());
 //                    Log.d("SendActivity", "p2pkh utxos:" + utxos.size());
         } else {
             utxos = APIFactory.getInstance(context).getUtxos(true);
