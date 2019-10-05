a9bc325c77ebe31479b22245ec4a381e9e487970
zeroleak <zeroleak@samourai.io>
2019-10-05 11:17:07 +0200

    update for whirlpool-client

--- app/src/main/java/com/samourai/whirlpool/client/tx0/AndroidTx0Service.java
+++ app/src/main/java/com/samourai/whirlpool/client/tx0/AndroidTx0Service.java
@@ -1,10 +1,12 @@
 package com.samourai.whirlpool.client.tx0;
 
 import com.samourai.stomp.client.AndroidStompClient;
+import com.samourai.wallet.api.backend.beans.UnspentResponse;
 import com.samourai.wallet.bip69.BIP69OutputComparator;
 import com.samourai.wallet.client.Bip84Wallet;
 import com.samourai.wallet.hd.HD_Address;
 import com.samourai.wallet.segwit.bech32.Bech32UtilGeneric;
+import com.samourai.wallet.util.FeeUtil;
 import com.samourai.whirlpool.client.wallet.WhirlpoolWalletConfig;
 import com.samourai.whirlpool.protocol.fee.WhirlpoolFee;
 
@@ -21,6 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -28,15 +31,34 @@ public class AndroidTx0Service extends Tx0Service {
     private Logger log = LoggerFactory.getLogger(AndroidTx0Service.class.getSimpleName());
 
     private final Bech32UtilGeneric bech32Util = Bech32UtilGeneric.getInstance();
+    private final FeeUtil feeUtil = FeeUtil.getInstance();
 
     public AndroidTx0Service(WhirlpoolWalletConfig config) {
         super(config);
     }
 
+    protected long computeTx0MinerFee(int nbPremix, long feeTx0, Collection<UnspentResponse.UnspentOutput> spendFroms) {
+        int nbOutputsNonOpReturn = nbPremix + 2; // outputs + change + fee
+        // spend from N bech32 input
+        long tx0MinerFee = feeUtil.estimatedFeeSegwit(0, 0, spendFroms.size(), nbOutputsNonOpReturn, 1, feeTx0);
+
+        if (log.isDebugEnabled()) {
+            log.debug(
+                    "tx0 minerFee: "
+                            + tx0MinerFee
+                            + "sats, totalBytes="
+                            + "b for nbPremix="
+                            + nbPremix
+                            + ", feeTx0="
+                            + feeTx0);
+        }
+        return tx0MinerFee;
+    }
+
     @Override
     protected Transaction buildTx0(
             byte[] spendFromPrivKey,
-            TransactionOutPoint depositSpendFrom,
+            Collection<UnspentResponse.UnspentOutput> depositSpendFroms,
             Bip84Wallet depositWallet,
             Bip84Wallet premixWallet,
             long premixValue,
@@ -157,14 +179,13 @@ public class AndroidTx0Service extends Tx0Service {
         ECKey spendFromKey = ECKey.fromPrivate(spendFromPrivKey);
 
         final Script segwitPubkeyScript = ScriptBuilder.createP2WPKHOutputScript(spendFromKey);
-        tx.addSignedInput(depositSpendFrom, segwitPubkeyScript, spendFromKey);
+
+        // TODO sign all depositSpendFroms
+        tx.addSignedInput(depositSpendFroms.iterator().next().computeOutpoint(params), segwitPubkeyScript, spendFromKey);
         if (log.isDebugEnabled()) {
             log.debug(
                     "Tx0 in: utxo="
-                            + depositSpendFrom
-                            + " ("
-                            + depositSpendFrom.getValue().getValue()
-                            + " sats)");
+                            + depositSpendFroms);
             log.debug("Tx0 fee: " + tx0MinerFee + " sats");
         }
         tx.verify();
