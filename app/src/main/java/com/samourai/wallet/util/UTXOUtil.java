128167e908c7f3680db4b4e645f946919d3996b7
TDevD <dev@samouraiwallet.com>
2019-11-26 17:26:49 +0100

    UTXOUtil: add support for utxo notes

--- app/src/main/java/com/samourai/wallet/util/UTXOUtil.java
+++ app/src/main/java/com/samourai/wallet/util/UTXOUtil.java
@@ -10,6 +10,7 @@ public class UTXOUtil {
     private static UTXOUtil instance = null;
 
     private static HashMap<String,String> utxoTags = null;
+    private static HashMap<String,String> utxoNotes = null;
 
     private UTXOUtil() { ; }
 
@@ -17,6 +18,7 @@ public class UTXOUtil {
 
         if(instance == null) {
             utxoTags = new HashMap<String,String>();
+            utxoNotes = new HashMap<String,String>();
             instance = new UTXOUtil();
         }
 
@@ -25,6 +27,7 @@ public class UTXOUtil {
 
     public void reset() {
         utxoTags.clear();
+        utxoNotes.clear();
     }
 
     public void add(String utxo, String tag) {
@@ -49,6 +52,28 @@ public class UTXOUtil {
         utxoTags.remove(utxo);
     }
 
+    public void addNote(String utxo, String note) {
+        utxoNotes.put(utxo, note);
+    }
+
+    public String getNote(String utxo) {
+        if(utxoNotes.containsKey(utxo))  {
+            return utxoNotes.get(utxo);
+        }
+        else    {
+            return null;
+        }
+
+    }
+
+    public HashMap<String,String> getNotes() {
+        return utxoNotes;
+    }
+
+    public void removeNote(String utxo) {
+        utxoNotes.remove(utxo);
+    }
+
     public JSONArray toJSON() {
 
         JSONArray utxos = new JSONArray();
@@ -57,6 +82,12 @@ public class UTXOUtil {
             tag.put(key);
             tag.put(utxoTags.get(key));
             utxos.put(tag);
+        }
+        for(String key : utxoNotes.keySet()) {
+            JSONArray note = new JSONArray();
+            note.put(key);
+            note.put(utxoNotes.get(key));
+            utxos.put(note);
         }
 
         return utxos;
@@ -67,6 +98,35 @@ public class UTXOUtil {
             for(int i = 0; i < utxos.length(); i++) {
                 JSONArray tag = (JSONArray)utxos.get(i);
                 utxoTags.put((String)tag.get(0), (String)tag.get(1));
+            }
+            for(int i = 0; i < utxos.length(); i++) {
+                JSONArray note = (JSONArray)utxos.get(i);
+                utxoNotes.put((String)note.get(0), (String)note.get(1));
+            }
+        }
+        catch(JSONException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public JSONArray toJSON_notes() {
+
+        JSONArray utxos = new JSONArray();
+        for(String key : utxoNotes.keySet()) {
+            JSONArray note = new JSONArray();
+            note.put(key);
+            note.put(utxoNotes.get(key));
+            utxos.put(note);
+        }
+
+        return utxos;
+    }
+
+    public void fromJSON_notes(JSONArray utxos) {
+        try {
+            for(int i = 0; i < utxos.length(); i++) {
+                JSONArray note = (JSONArray)utxos.get(i);
+                utxoNotes.put((String)note.get(0), (String)note.get(1));
             }
         }
         catch(JSONException ex) {
