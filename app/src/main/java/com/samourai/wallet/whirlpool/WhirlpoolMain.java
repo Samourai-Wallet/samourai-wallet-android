c0efb00eb10e822a2ecf6f74d7149ab7bd3248f0
Sarath <sarathkumars677@gmail.com>
2019-12-04 19:23:20 +0530

    Display whirlpool balance in dashboard

--- app/src/main/java/com/samourai/wallet/whirlpool/WhirlpoolMain.java
+++ app/src/main/java/com/samourai/wallet/whirlpool/WhirlpoolMain.java
@@ -3,7 +3,6 @@ package com.samourai.wallet.whirlpool;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,24 +16,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.samourai.wallet.R;
+import com.samourai.wallet.api.APIFactory;
 import com.samourai.wallet.util.AppUtil;
-import com.samourai.wallet.whirlpool.fragments.WhirlpoolCyclesFragment;
 import com.samourai.wallet.whirlpool.models.Cycle;
 import com.samourai.wallet.whirlpool.newPool.DepositOrChooseUtxoDialog;
 import com.samourai.wallet.widgets.ItemDividerDecorator;
-import com.samourai.wallet.widgets.ViewPager;
+
+import org.bitcoinj.core.Coin;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class WhirlpoolMain extends AppCompatActivity {
 
-    private RecyclerView CycleRecyclerView;
-    private WhirlpoolCyclesFragment dashboard, inProgressCycles, completedCycles;
     private ArrayList<Cycle> cycles = new ArrayList();
     private String tabTitle[] = {"Dashboard", "In Progress", "Completed"};
-    private ViewPager cyclesViewPager;
-    private TabLayout cyclesTabLayout;
     private RecyclerView mixList;
     private TextView totalAmountToDisplay;
     private TextView amountSubText;
@@ -53,60 +49,9 @@ public class WhirlpoolMain extends AppCompatActivity {
         totalAmountToDisplay = findViewById(R.id.whirlpool_total_amount_to_display);
         amountSubText = findViewById(R.id.toolbar_subtext);
         mixList = findViewById(R.id.rv_whirlpool_dashboard);
-//        cyclesViewPager = findViewById(R.id.whirlpool_viewpager);
-//        cyclesTabLayout = findViewById(R.id.whirlpool_home_tabs);
-//        cyclesTabLayout.setupWithViewPager(cyclesViewPager);
-//        dashboard = new WhirlpoolCyclesFragment();
-//        inProgressCycles = new WhirlpoolCyclesFragment();
-//        completedCycles = new WhirlpoolCyclesFragment();
-//        cyclesViewPager.enableSwipe(true);
-//
-//        CyclesViewPagerAdapter adapter = new CyclesViewPagerAdapter(getSupportFragmentManager());
-//        cyclesViewPager.setAdapter(adapter);
-//        cyclesViewPager.setCurrentItem(1);
         findViewById(R.id.whirlpool_fab).setOnClickListener(view -> {
             showBottomSheetDialog();
         });
-//
-//        cyclesViewPager.addOnPageChangeListener(new android.support.v4.view.ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//                switch (position) {
-//                    //Dashboard
-//                    case 0: {
-//                        amountSubText.setText(R.string.total_whirlpool_balance);
-//                        long value = APIFactory.getInstance(WhirlpoolMain.this).getXpubBalance();
-//                        totalAmountToDisplay.setText(Coin.valueOf(value).toPlainString() + " BTC");
-//                        break;
-//                    }
-//
-//                    case 1: {
-//                        //In-Progress
-//                        amountSubText.setText(R.string.premix_balance);
-//                        long value = APIFactory.getInstance(WhirlpoolMain.this).getXpubPreMixBalance();
-//                        totalAmountToDisplay.setText(Coin.valueOf(value).toPlainString() + " BTC");
-//                        break;
-//                    }
-//
-//                    case 2: {
-//                        //Completed
-//                        amountSubText.setText(R.string.post_mix_balance);
-//                        long value = APIFactory.getInstance(WhirlpoolMain.this).getXpubPostMixBalance();
-//                        totalAmountToDisplay.setText(Coin.valueOf(value).toPlainString() + " BTC");
-//                        break;
-//                    }
-//
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) { }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) { }
-//        });
         mixList.setLayoutManager(new LinearLayoutManager(this));
 
         adapter = new MixAdapter(new ArrayList<>());
@@ -115,6 +60,10 @@ public class WhirlpoolMain extends AppCompatActivity {
         mixList.addItemDecoration(new ItemDividerDecorator(drawable));
         mixList.setAdapter(adapter);
 
+        long postMixBalance = APIFactory.getInstance(WhirlpoolMain.this).getXpubPostMixBalance();
+        long preMixBalance = APIFactory.getInstance(WhirlpoolMain.this).getXpubPreMixBalance();
+
+        totalAmountToDisplay.setText(Coin.valueOf(postMixBalance + preMixBalance).toPlainString().concat(" BTC"));
     }
 
     private void showBottomSheetDialog() {
