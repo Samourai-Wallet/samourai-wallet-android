5ee9fa20a0385ce58506d832a41385db5619e6d6
Sarath <sarathkumars677@gmail.com>
2020-12-29 23:08:03 +0530

    new activity for batch spend

---
+++ app/src/main/java/com/samourai/wallet/send/batch/BatchSpendActivity.kt
@@ -0,0 +1,13 @@
+package com.samourai.wallet.send.batch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.samourai.wallet.R
+import com.samourai.wallet.SamouraiActivity
+
+class BatchSpendActivity : SamouraiActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_batch_spend)
+    }
+}
