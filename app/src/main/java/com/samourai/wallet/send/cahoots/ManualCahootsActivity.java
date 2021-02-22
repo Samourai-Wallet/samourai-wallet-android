af1ebbee5c6b2f34a7dc18d875b22d83b75cf361
Sarath <sarathkumars677@gmail.com>
2021-02-22 22:40:02 +0530

    [UPDATE] Bottomsheet QR code for cahoots

--- app/src/main/java/com/samourai/wallet/send/cahoots/ManualCahootsActivity.java
+++ app/src/main/java/com/samourai/wallet/send/cahoots/ManualCahootsActivity.java
@@ -1,32 +1,21 @@
 package com.samourai.wallet.send.cahoots;
 
-import android.app.AlertDialog;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.InputType;
-import android.view.Display;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.android.Contents;
-import com.google.zxing.client.android.encode.QRCodeEncoder;
+import androidx.appcompat.app.AppCompatDialog;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.samourai.soroban.cahoots.CahootsContext;
 import com.samourai.soroban.cahoots.ManualCahootsMessage;
 import com.samourai.soroban.cahoots.ManualCahootsService;
@@ -40,14 +29,7 @@ import com.samourai.wallet.cahoots.CahootsType;
 import com.samourai.wallet.cahoots.CahootsTypeUser;
 import com.samourai.wallet.cahoots.psbt.PSBT;
 import com.samourai.wallet.util.AppUtil;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import androidx.appcompat.app.AppCompatDialog;
-import androidx.core.content.FileProvider;
+import com.samourai.wallet.util.QRBottomSheetDialog;
 
 public class ManualCahootsActivity extends SamouraiActivity {
     private static final String TAG = "ManualCahootsActivity";
@@ -198,107 +180,29 @@ public class ManualCahootsActivity extends SamouraiActivity {
     private void shareCahootsPayload() throws Exception {
         String strCahoots = this.cahootsUi.getCahootsMessage().toPayload();
         final int QR_ALPHANUM_CHAR_LIMIT = 4296;    // tx max size in bytes == 2148
-        dialog = new AppCompatDialog(this, R.style.stowaway_dialog);
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        dialog.setCancelable(true);
-        dialog.setContentView(R.layout.cahoots_qr_dialog_layout);
-        ImageView qrCode = dialog.findViewById(R.id.qr_code_imageview);
-        Button copy = dialog.findViewById(R.id.cahoots_copy_btn);
-        Button share = dialog.findViewById(R.id.cahoots_share);
-        TextView qrErrorMessage = dialog.findViewById(R.id.qr_error_stowaway);
-
         if(strCahoots==null){
             return;
         }
         if (strCahoots != null && strCahoots.length() <= QR_ALPHANUM_CHAR_LIMIT) {
-            Display display = this.getWindowManager().getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
-            int imgWidth = Math.max(size.x - 240, 150);
-
-            Bitmap bitmap = null;
-
-            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder(strCahoots, null, Contents.Type.TEXT, BarcodeFormat.QR_CODE.toString(), imgWidth);
-
-            try {
-                bitmap = qrCodeEncoder.encodeAsBitmap();
-            } catch (WriterException e) {
-                qrErrorMessage.setVisibility(View.VISIBLE);
-                e.printStackTrace();
-            }
-            qrCode.setImageBitmap(bitmap);
         } else {
-            qrErrorMessage.setVisibility(View.VISIBLE);
             Toast.makeText(getApplicationContext(), R.string.tx_too_large_qr, Toast.LENGTH_SHORT).show();
         }
-        share.setOnClickListener(v -> {
 
-            try {
-                if (!(strCahoots.length() <= QR_ALPHANUM_CHAR_LIMIT)) {
-                    Intent txtIntent = new Intent(android.content.Intent.ACTION_SEND);
-                    txtIntent.setType("text/plain");
-                    txtIntent.putExtra(android.content.Intent.EXTRA_TEXT, strCahoots);
-                    startActivity(Intent.createChooser(txtIntent, "Share"));
-                    return;
-                }
-                String strFileName = AppUtil.getInstance(getApplicationContext()).getReceiveQRFilename();
-                File file = new File(strFileName);
-                if (!file.exists()) {
-                    try {
-                        file.createNewFile();
-                    } catch (Exception e) {
-                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                }
-                file.setReadable(true, false);
+        if (!(strCahoots.length() <= QR_ALPHANUM_CHAR_LIMIT)) {
+            Intent txtIntent = new Intent(android.content.Intent.ACTION_SEND);
+            txtIntent.setType("text/plain");
+            txtIntent.putExtra(android.content.Intent.EXTRA_TEXT, strCahoots);
+            startActivity(Intent.createChooser(txtIntent, "Share"));
+            return;
+        }
+        QRBottomSheetDialog dialog = new QRBottomSheetDialog(
+              strCahoots,
+             "Share",
+                "payload"
 
-                FileOutputStream fos = null;
+        );
 
-                fos = new FileOutputStream(file);
-
-
-                if (file != null && fos != null) {
-                    Bitmap bitmap1 = ((BitmapDrawable) qrCode.getDrawable()).getBitmap();
-                    bitmap1.compress(Bitmap.CompressFormat.PNG, 0, fos);
-
-                    try {
-                        fos.close();
-                    } catch (IOException ioe) {
-                        ;
-                    }
-
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_SEND);
-                    intent.setType("image/png");
-                    if (android.os.Build.VERSION.SDK_INT >= 24) {
-                        //From API 24 sending FIle on intent ,require custom file provider
-                        intent.putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(
-                                getApplicationContext(),
-                                getApplicationContext().getApplicationContext()
-                                        .getPackageName() + ".provider", file));
-                    } else {
-                        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
-                    }
-                    ManualCahootsActivity.this.startActivity(Intent.createChooser(intent, getApplicationContext().getText(R.string.send_tx)));
-                }
-
-            } catch (Exception ignored) {
-                    Toast.makeText(this,getString(R.string.cannot_compose_cahoots),Toast.LENGTH_SHORT).show();
-            }
-
-        });
-
-        copy.setOnClickListener(v -> {
-
-            android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getApplicationContext().getSystemService(android.content.Context.CLIPBOARD_SERVICE);
-            android.content.ClipData clip = null;
-            clip = android.content.ClipData.newPlainText("Cahoots", strCahoots);
-            clipboard.setPrimaryClip(clip);
-            Toast.makeText(getApplicationContext(), R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
-
-        });
-
-        dialog.show();
+        dialog.show(getSupportFragmentManager(), dialog.getTag());
 
     }
 
@@ -321,19 +225,17 @@ public class ManualCahootsActivity extends SamouraiActivity {
             tvHexTx.setText(strPSBT);
             tvHexTx.setPadding((int) (8 * scale + 0.5f), (int) (6 * scale + 0.5f), (int) (8 * scale + 0.5f), (int) (6 * scale + 0.5f));
 
-            AlertDialog.Builder dlg = new AlertDialog.Builder(ManualCahootsActivity.this)
+            MaterialAlertDialogBuilder dlg = new MaterialAlertDialogBuilder(ManualCahootsActivity.this)
                     .setTitle(R.string.app_name)
                     .setView(tvHexTx)
                     .setCancelable(false)
-                    .setPositiveButton(R.string.copy_to_clipboard, new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                            dialog.dismiss();
-                            android.content.ClipboardManager clipboard = (android.content.ClipboardManager)getSystemService(android.content.Context.CLIPBOARD_SERVICE);
-                            android.content.ClipData clip = null;
-                            clip = android.content.ClipData.newPlainText("tx", strPSBT);
-                            clipboard.setPrimaryClip(clip);
-                            Toast.makeText(ManualCahootsActivity.this, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
-                        }
+                    .setPositiveButton(R.string.copy_to_clipboard, (dialog, whichButton) -> {
+                        dialog.dismiss();
+                        ClipboardManager clipboard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+                        ClipData clip = null;
+                        clip = ClipData.newPlainText("tx", strPSBT);
+                        clipboard.setPrimaryClip(clip);
+                        Toast.makeText(ManualCahootsActivity.this, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
                     })
                     .setNegativeButton(R.string.close, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int whichButton) {
