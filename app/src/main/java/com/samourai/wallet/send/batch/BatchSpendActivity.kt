4a9c443ce64d20b9473fb0e44600657e2b1cb79a
Sarath <sarathkumars677@gmail.com>
2021-03-06 13:32:50 +0530

    [update] improved fee rate calculation in batch activity

--- app/src/main/java/com/samourai/wallet/send/batch/BatchSpendActivity.kt
+++ app/src/main/java/com/samourai/wallet/send/batch/BatchSpendActivity.kt
@@ -613,9 +613,9 @@ class BatchSpendActivity : SamouraiActivity() {
                 .replace(R.id.batchDetailContainer, reviewFragment)
                 .commit()
         isInReviewMode = true
-        this.doSpend()
+
         reviewFragment.setOnFeeChangeListener {
-            this.doSpend()
+            this.prepareSpend()
         }
         reviewFragment.setOnClickListener {
             this.initiateSpend()
@@ -673,7 +673,7 @@ class BatchSpendActivity : SamouraiActivity() {
 
     }
 
-    fun doSpend() {
+    fun prepareSpend() {
         //Resets current receivers,outpoints etc..
         this.reset()
         for (_data in viewModel.getBatchList()) {
@@ -799,7 +799,8 @@ class BatchSpendActivity : SamouraiActivity() {
             } else {
                 rbf = null
             }
-
+            val signedTx = SendFactory.getInstance(application).signTransaction(tx, account)
+            reviewFragment.setFeeRate(fee.toDouble() / signedTx.virtualTransactionSize)
             reviewFragment.setTotalMinerFees(fee)
         }
     }
@@ -808,12 +809,10 @@ class BatchSpendActivity : SamouraiActivity() {
     private fun initiateSpend() {
         val strMessage = "Send ${FormatsUtil.getBTCDecimalFormat(amount.toLong())} BTC. (fee: ${FormatsUtil.getBTCDecimalFormat(fee.toLong())})"
 
-        val _change_address = change_address
         val _change_idx = change_idx
         val _amount = amount
         var SignedTx = SendFactory.getInstance(applicationContext).signTransaction(tx, 0)
         val hexTx = String(Hex.encode(SignedTx.bitcoinSerialize()))
-        val strTxHash = SignedTx.hashAsString
 
         val dlg = MaterialAlertDialogBuilder(this)
                 .setTitle(R.string.app_name)
