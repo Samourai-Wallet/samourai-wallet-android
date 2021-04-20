8f77a375031b9e3aa9c115cfdce38a2412ad5726
Sarath <sarathkumars677@gmail.com>
2021-04-20 18:07:30 +0530

    [Update] Use window color for statusbar bg in restore activity

--- app/src/main/java/com/samourai/wallet/RestoreSeedWalletActivity.java
+++ app/src/main/java/com/samourai/wallet/RestoreSeedWalletActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.os.Looper;
+
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -89,6 +91,7 @@ public class RestoreSeedWalletActivity extends FragmentActivity implements
             restoreMode = getIntent().getStringExtra("mode");
             setUpAdapter();
         }
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.window));
 
         String BIP39_EN = null;
         StringBuilder sb = new StringBuilder();
