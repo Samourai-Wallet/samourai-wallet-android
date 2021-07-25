f425cd24668071cbaa249afc77ebcfa67ec1b068
Sarath <sarathkumars677@gmail.com>
2021-07-25 13:33:33 +0530

    Clean up

--- app/src/main/java/com/samourai/wallet/home/BalanceActivity.java
+++ app/src/main/java/com/samourai/wallet/home/BalanceActivity.java
@@ -312,7 +312,6 @@ public class BalanceActivity extends SamouraiActivity {
         balanceViewModel = ViewModelProviders.of(this).get(BalanceViewModel.class);
         balanceViewModel.setAccount(account);
 
-        ExternalBackupManager.askPermission(this);
 
         makePaynymAvatarCache();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
